166404e4faa43453c23f18bb126ab4b495721f54
yuzhijun <664281688@qq.com>
2016-04-25 14:58:15 +0800

    增加内存泄漏检测

---
+++ app/src/main/java/com/medical/lenovo/lastframework/base/BaseApplication.java
@@ -0,0 +1,26 @@
+package com.medical.lenovo.lastframework.base;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+/**
+ * Created by yuzhijun on 2016/4/25.
+ */
+public class BaseApplication extends Application {
+
+    public static RefWatcher getRefWatcher(Context context) {
+        BaseApplication application = (BaseApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
+    private RefWatcher refWatcher;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        refWatcher = LeakCanary.install(this);
+    }
+}
